afd9455760f3f2360a219e1a4b19b6806665096f
Squiddle <arcb@protonmail.com>
2020-01-21 20:36:01 -0800

    first commit

---
+++ lab1.py
@@ -0,0 +1,42 @@
+# This statement will say Hello world!
+
+''' This is
+a 
+multi-line
+comment '''
+
+#print('Hello world!')
+
+#variables are case sensitive. As a rule of thumb, it is best to have them start with lower case letters. Cannot start with numbers
+#greeting = 'I hope you are all having a great day!'
+#print(greeting)
+
+#mouse = "Mickey"
+#cat = "Garfield"
+#llama123 = "spit"
+# 123llama = "spit" not ok- cannot start with numbers
+#llama_235 = "spit" # an use underscores with numbers
+
+#print(mouse, cat)
+
+#print(f"{mouse} was a mouse who had a house {cat}") # f is for "format string"
+
+#number = input('Enter your grade ')
+#number = int(number) # cast
+#number = str(number)
+
+#print(number + " " + 'applesauce')
+'''
+# prints a question to the user
+# then allows the user to type an input
+# saves user input to a variable
+user_name = input("What is your name?") # this line has three actions!
+
+# prints "Hello" with the user's name that they typed in!
+print(f"Hello {user_name}!") # outcome: "Hello Lisa"
+
+'''
+
+user_name = input("What is your name? ")
+print(f'hello {user_name}' )
+
---
+++ lab2.py
@@ -0,0 +1,18 @@
+
+adjective1 = input("Give me an adjective: ")
+sport = input("Name a sport: ")
+city = input("Name a city: ")
+noun1 = input("Name a noun: ")
+noun2 = input("Name a noun: ")
+action_verb = input("Give me an action verb: ")
+noun3 = input("Another noun: ")
+noun4 = input("And another noun: ")
+noun5 = input("One more noun: ")
+adjective2 = input("Give me an adjective: ")
+noun6 = input("Give me a noun: ")
+adjective3 = input("One last adjective: ")
+noun7 = input("And one last noun: ")
+
+madlib = f'One day my {adjective1} friend and I decided to go to the {sport} game in {city}. We really wanted to see the {noun1} play the {noun2}. So we {action_verb} our {noun3} down to the {noun4} and bought some {noun5}. We got into the game and it was a lot of fun. We ate some {adjective2} {noun6} and drank some {adjective3} {noun7}. We had a great time! We plan to go again next year!'
+
+print(madlib)
