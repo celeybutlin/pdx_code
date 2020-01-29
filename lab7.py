c80ec43cc7c4523f0212645763db7c17bdc37c6b
Ashley Celey-Butlin <arcb@protonmail.com>
2020-01-28 20:05:56 -0800

    changed list to a range function of a list

--- lab7.py
+++ lab7.py
@@ -8,13 +8,12 @@ ADVANCED VERSION 2: Let them keep guessing in a while loop. Keep track of how ma
 import random
 
 user_guess = int(input("Guess the number: "))
-num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+num = list(range(1,11))
+computer_choice = random.choice(num)
 count = 0
 
 while True:
     count += 1
-    computer_choice = random.choice(num)
     if user_guess == computer_choice:
         print("Correct!")
         break
@@ -23,8 +22,13 @@ while True:
         user_guess = int(input("Guess the number: "))
         continue
     else:
-        print("Wrong! You are too high!")
+        print("Wrong! You are too high! Let's play again.")
         user_guess = int(input("Guess the number: "))
         continue
 
 print(f'You guessed {count} times')
+
+
+
+
+    
